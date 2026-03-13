657492d6d52bc97c5ebaa13aa525a013a57c825a
Andrew Kane <andrew@ankane.org>
2026-03-13 01:43:08 -0700

    Added tests for private functions

--- test/main.c
+++ test/main.c
@@ -180,6 +180,37 @@ void test_count_negative_exposure(void) {
     assert(status == -1);
 }
 
+void test_prob_b_beats_a(void) {
+    assert_approx(bayestest_prob_b_beats_a(1, 2, 3, 4), 0.6428571428571429);
+    assert_approx(bayestest_prob_b_beats_a(55, 50, 30, 30), 0.38386463776317903);
+    assert_approx(bayestest_prob_b_beats_a(50, 50, 35, 30), 0.6867997222295887);
+}
+
+void test_prob_c_beats_ab(void) {
+    assert_approx(bayestest_prob_c_beats_ab(1, 2, 3, 4, 5, 6), 0.4128959276018096);
+    assert_approx(bayestest_prob_c_beats_ab(1, 2, 3, 4, 5, 100), 0.0004147782900546515);
+    assert_approx(bayestest_prob_c_beats_ab(55, 50, 30, 30, 10, 10), 0.35421204276865736);
+    assert_approx(bayestest_prob_c_beats_ab(50, 50, 35, 30, 13, 18), 0.09139305107602524);
+}
+
+void test_prob_d_beats_abc(void) {
+    assert_approx(bayestest_prob_d_beats_abc(1, 2, 3, 4, 5, 6, 7, 8), 0.2853316096371013);
+    assert_approx(bayestest_prob_d_beats_abc(55, 50, 30, 30, 10, 10, 25, 30), 0.08421499131901738);
+}
+
+void test_prob_1_beats_2(void) {
+    assert_approx(bayestest_prob_1_beats_2(1, 2, 3, 4), 0.29629629629629595);
+    assert_approx(bayestest_prob_1_beats_2(55, 50, 30, 30), 0.6710529663661625);
+    assert_approx(bayestest_prob_1_beats_2(50, 50, 35, 30), 0.24796547380927997);
+}
+
+void test_prob_1_beats_23(void) {
+    assert_approx(bayestest_prob_1_beats_23(1, 2, 3, 4, 5, 6), 0.16901765046296247);
+    assert_approx(bayestest_prob_1_beats_23(1, 2, 3, 4, 5, 100), 0.2962330601144884);
+    assert_approx(bayestest_prob_1_beats_23(55, 50, 30, 30, 10, 10), 0.4633365654508068);
+    assert_approx(bayestest_prob_1_beats_23(50, 50, 35, 30, 13, 18), 0.23397153850438435);
+}
+
 int main(void) {
     test_binary_no_variants();
     test_binary_one_variant();
@@ -201,4 +232,13 @@ int main(void) {
     test_count_exposure_relative();
     test_count_negative_events();
     test_count_negative_exposure();
+
+    test_prob_b_beats_a();
+    test_prob_c_beats_ab();
+    test_prob_d_beats_abc();
+
+    test_prob_1_beats_2();
+    test_prob_1_beats_23();
+
+    return 0;
 }
