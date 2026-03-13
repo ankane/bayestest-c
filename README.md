763838becd46dc8a8fdc7c52461e181d26db19b0
Andrew Kane <andrew@ankane.org>
2026-03-12 21:26:20 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# BayesTest C
+
+Bayesian A/B testing calculations for C
+
+Based on [this post](https://www.evanmiller.org/bayesian-ab-testing.html) by Evan Miller
+
+Also available in [C++](https://github.com/ankane/bayestest-cpp) and [Rust](https://github.com/ankane/bayestest-rust)
+
+[![Build Status](https://github.com/ankane/bayestest-c/actions/workflows/build.yml/badge.svg)](https://github.com/ankane/bayestest-c/actions)
+
+## Installation
+
+Add [the header](https://raw.githubusercontent.com/ankane/bayestest-c/v0.1.0/include/bayestest.h) to your project.
+
+## Getting Started
+
+Include the header
+
+```c
+#include "bayestest.h"
+```
+
+Get the winning probability of each variant for binary outcomes
+
+```c
+int participants[2] = {100, 200};
+int conversions[2] = {50, 95};
+double probabilities[2];
+int status = bayestest_binary(2, participants, conversions, probabilities);
+```
+
+Status is `0` on success
+
+Supports up to 4 variants
+
+## History
+
+View the [changelog](https://github.com/ankane/bayestest-c/blob/master/CHANGELOG.md)
+
+## Contributing
+
+Everyone is encouraged to help improve this project. Here are a few ways you can help:
+
+- [Report bugs](https://github.com/ankane/bayestest-c/issues)
+- Fix bugs and [submit pull requests](https://github.com/ankane/bayestest-c/pulls)
+- Write, clarify, or fix documentation
+- Suggest or add new features
+
+To get started with development:
+
+```sh
+git clone https://github.com/ankane/bayestest-c.git
+cd bayestest-c
+cmake -S . -B build
+cmake --build build
+build/test
+```
---
+++ test/main.c
@@ -0,0 +1,94 @@
+#include <assert.h>
+#include <math.h>
+
+#include <bayestest.h>
+
+void assert_approx(double act, double exp) {
+    assert(fabs(act - exp) < 0.0000000001);
+}
+
+void test_binary_no_variants(void) {
+    int participants[1] = {0};
+    int conversions[1] = {0};
+    double probabilities[1] = {99};
+    int status = bayestest_binary(0, participants, conversions, probabilities);
+    assert(status == 0);
+    assert(probabilities[0] == 99);
+}
+
+void test_binary_one_variant(void) {
+    int participants[1] = {2};
+    int conversions[1] = {1};
+    double probabilities[1];
+    int status = bayestest_binary(1, participants, conversions, probabilities);
+    assert(status == 0);
+    assert(probabilities[0] == 1);
+}
+
+void test_binary_two_variants(void) {
+    int participants[2] = {200, 400};
+    int conversions[2] = {100, 250};
+    double probabilities[2];
+    int status = bayestest_binary(2, participants, conversions, probabilities);
+    assert(status == 0);
+    assert_approx(probabilities[0], 0.001756431311879969);
+    assert_approx(probabilities[1], 0.99824356868812);
+}
+
+void test_binary_three_variants(void) {
+    int participants[3] = {61, 54, 72};
+    int conversions[3] = {15, 13, 19};
+    double probabilities[3];
+    int status = bayestest_binary(3, participants, conversions, probabilities);
+    assert(status == 0);
+    assert_approx(probabilities[0], 0.29632930651329037);
+    assert_approx(probabilities[1], 0.277257277195332);
+    assert_approx(probabilities[2], 0.42641341629137763);
+}
+
+void test_binary_four_variants(void) {
+    int participants[4] = {55, 30, 10, 50};
+    int conversions[4] = {50, 30, 10, 45};
+    double probabilities[4];
+    int status = bayestest_binary(4, participants, conversions, probabilities);
+    assert(status == 0);
+    assert_approx(probabilities[0], 0.02692341639320739);
+    assert_approx(probabilities[1], 0.7040521621641954);
+    assert_approx(probabilities[2], 0.249824960767943);
+    assert_approx(probabilities[3], 0.019199460674668434);
+}
+
+void test_binary_negative_participants(void) {
+    int participants[1] = {-1};
+    int conversions[1] = {1};
+    double probabilities[1];
+    int status = bayestest_binary(1, participants, conversions, probabilities);
+    assert(status == -1);
+}
+
+void test_binary_negative_conversions(void) {
+    int participants[1] = {1};
+    int conversions[1] = {-1};
+    double probabilities[1];
+    int status = bayestest_binary(1, participants, conversions, probabilities);
+    assert(status == -1);
+}
+
+void test_binary_high_conversions(void) {
+    int participants[1] = {1};
+    int conversions[1] = {2};
+    double probabilities[1];
+    int status = bayestest_binary(1, participants, conversions, probabilities);
+    assert(status == -1);
+}
+
+int main(void) {
+    test_binary_no_variants();
+    test_binary_one_variant();
+    test_binary_two_variants();
+    test_binary_three_variants();
+    test_binary_four_variants();
+    test_binary_negative_participants();
+    test_binary_negative_conversions();
+    test_binary_high_conversions();
+}
